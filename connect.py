57cbfb4774f0fcb34bb2115008519f8c9bf601a5
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-12-04 19:43:07 +0800

    database config

---
+++ connect.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import sqlite3
+
+window = tk.Tk()
+
+window.title("DATABASE")
+
+con = sqlite3.connect("bus_ticket_DB.db")
+
+c = con.cursor()
+
+# #Admin Details Table
+c.execute("""CREATE TABLE admin_credentials (
+    uname text NOT NULL,
+    pword text NOT NULL
+    )
+    """)
+
+
+# Insert data into the admin_credentials table
+admin_insert = [
+    ('angelica', '123'),
+    ('tracy', 'babi')
+]
+
+for data in admin_insert:
+    c.execute("INSERT INTO admin_credentials VALUES (?, ?)", data)
+
+
+#Bus Details Table
+c.execute("""CREATE TABLE bus_details (
+    bus_num text NOT NULL,
+    plate_num text NOT NULL,
+    driver_name int NOT NULL,
+    contact_num int NOT NULL,
+    capacity int NOT NULL,
+    destination text NOT NULL
+    )
+    """)
+
+#Passenger Details table
+c.execute("""CREATE TABLE passenger_details (
+    passenger_name text NOT NULL,
+    address text NOT NULL,
+    contact_num int NOT NULL
+    )
+    """)
+
+con.commit()
+con.close()
+
+window.mainloop()
