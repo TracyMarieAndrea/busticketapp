f2d5543f2f883bbc3b75463d035707859b84e45e
Jingkay Simbran <angelicagrace.simbran@wvsu.edu.ph>
2023-12-08 15:22:39 +0800

    delete and reload/refresh function

--- bus_details_form.py
+++ bus_details_form.py
@@ -7,12 +7,9 @@ import sqlite3
 
 class bus_details_form(object):
     def __init__(self, window):
-        window = tk.Tk()
-        window.title('Add Bus')
-        width = window.winfo_screenwidth()
-        height = window.winfo_screenheight()
-
-        window.geometry("%dx%d" % (width, height))
+        bus_details_window = tk.Tk()
+        bus_details_window.title('Add Bus')
+    
 
         def save_bus_details():
             con = sqlite3.connect('bus_ticket_DB.db')
@@ -29,71 +26,74 @@ class bus_details_form(object):
             )
             con.commit()
             con.close()
+            bus_details_window.destroy()
+            
+
 
 
         #Welcome Text
-        welcome_label = tk.Label(window,text="INPUT BUS DETAILS",font=('Roboto', 35,'bold'),fg='black')
+        welcome_label = tk.Label(bus_details_window,text="INPUT BUS DETAILS",font=('Roboto', 35,'bold'),fg='black')
         welcome_label.pack(pady=15)
 
         #Bus No. Text
-        bus_num_label = tk.Label(window,text="Bus Number",font=('Roboto', 12,),fg='black')
+        bus_num_label = tk.Label(bus_details_window,text="Bus Number",font=('Roboto', 12,),fg='black')
         bus_num_label.pack(pady=10)
         # bus_no_label.place(rely=0.35,relx=0.60)
 
         #Bus No. Entry
-        bus_num_entry = tk.Entry(window)
+        bus_num_entry = tk.Entry(bus_details_window)
         bus_num_entry.pack(pady=5)
         # bus_no_entry.place(rely=0.40,relx=0.60,width=230,height=30)
 
         #Plate Number Text
-        plate_num_label = tk.Label(window,text="Plate Number",font=('Roboto', 12,),fg='black')
+        plate_num_label = tk.Label(bus_details_window,text="Plate Number",font=('Roboto', 12,),fg='black')
         plate_num_label.pack(pady=10)
         # password_label.place(rely=0.50,relx=0.60)
 
         #Plate Number Entry
-        plate_num_entry = tk.Entry(window)
+        plate_num_entry = tk.Entry(bus_details_window)
         plate_num_entry.pack(pady=5)
         # password_entry.place(rely=0.55,relx=0.60,width=230,height=30)
 
         #Driver Name Text
-        driver_name_label = tk.Label(window,text="Driver Name",font=('Roboto', 12,),fg='black')
+        driver_name_label = tk.Label(bus_details_window,text="Driver Name",font=('Roboto', 12,),fg='black')
         driver_name_label.pack(pady=10)
 
         #Driver Name Entry
-        driver_name_entry = tk.Entry(window)
+        driver_name_entry = tk.Entry(bus_details_window)
         driver_name_entry.pack(pady=5)
 
         #Contact Number Text
-        contact_num_label = tk.Label(window,text="Contact Number",font=('Roboto', 12,),fg='black')
+        contact_num_label = tk.Label(bus_details_window,text="Contact Number",font=('Roboto', 12,),fg='black')
         contact_num_label.pack(pady=10)
 
         #Contact Number Entry
-        contact_num_entry = tk.Entry(window)
+        contact_num_entry = tk.Entry(bus_details_window)
         contact_num_entry.pack(pady=5)
 
         #Capacity Text
-        capacity_label = tk.Label(window,text="Capacity",font=('Roboto', 12,),fg='black')
+        capacity_label = tk.Label(bus_details_window,text="Capacity",font=('Roboto', 12,),fg='black')
         capacity_label.pack(pady=10)
         # password_label.place(rely=0.50,relx=0.60)
 
         #Capacity Entry
-        capacity_entry = tk.Entry(window)
+        capacity_entry = tk.Entry(bus_details_window)
         capacity_entry.pack(pady=5)
 
 
         #Destination Text
-        destination_label = tk.Label(window,text="Destination",font=('Roboto', 12,),fg='black')
+        destination_label = tk.Label(bus_details_window,text="Destination",font=('Roboto', 12,),fg='black')
         destination_label.pack(pady=10)
 
         #Destination Entry
-        destination_entry = tk.Entry(window)
+        destination_entry = tk.Entry(bus_details_window)
         destination_entry.pack(pady=5)
 
         #Save Button
-        save_btn = tk.Button(window, text="S A V E",background="#52ACA1",border=0, command=save_bus_details)
+        save_btn = tk.Button(bus_details_window, text="S A V E",background="#52ACA1",border=0, command=save_bus_details)
         save_btn.pack(pady=10)
         # save_btn.place(rely=0,relx=0,height=25,width=230)
 
 
 
-        window.mainloop()
+        bus_details_window.mainloop()
--- bus_management.py
+++ bus_management.py
@@ -22,6 +22,67 @@ class bus_management(object):
             show = bus_management(bus_management_window)
         def proceed_bus_details_form():
             show = bus_details_form(bus_management_window)
+        def delete_record():
+        # Check if a row is selected in the Treeview
+            selected_item = tree.selection()
+            if not selected_item:
+                messagebox.showwarning(bus_management_window,"Warning", "Please select a record to delete.")
+                return
+
+            # Get the values of the selected row
+            values = tree.item(selected_item)['values']
+
+            # Delete the selected row from the Treeview
+            tree.delete(selected_item)
+
+            # Create a database or connect to one that exists
+            conn = sqlite3.connect('bus_ticket_DB.db')
+
+            # Create a cursor instance
+            c = conn.cursor()
+
+            try:
+                # Delete From Database using values from the selected row
+                c.execute("DELETE FROM bus_details WHERE plate_num=?", (values[1],))
+                
+                # Commit changes
+                conn.commit()
+
+                # Add a little message box for fun
+                messagebox.showinfo(bus_management_window,"Deleted!", "Bus Record Has Been Deleted!")
+
+            except Exception as e:
+                # If an error occurs, rollback the changes and show an error message
+                conn.rollback()
+                messagebox.showerror(bus_management_window,"Error", f"Error deleting record: {str(e)}")
+
+            finally:
+                # Close our connection
+                conn.close()
+
+       
+        def refresh_table():
+            # Clear existing rows in the table
+            for item in tree.get_children():
+                tree.delete(item)
+
+            # Fetch all records from the database
+            con = sqlite3.connect("bus_ticket_DB.db")
+            c = con.cursor()
+            c.execute("SELECT * FROM bus_details")
+            result = c.fetchall()
+
+            for row in result:
+                tree.insert("", "end", values=row)
+
+            # vertical scrollbar
+            vsb = ttk.Scrollbar(bus_management_window, orient="vertical", command=tree.yview)
+            vsb.pack(side="right", fill="y")
+            tree.configure(yscrollcommand=vsb.set)
+
+            # Pack the Treeview widget
+            tree.pack(expand=True, fill="both")
+        
 
 
         #Welcome Text
@@ -43,15 +104,19 @@ class bus_management(object):
         edit_button = tk.Button(button_frame, text="Edit", command=addbus)
         edit_button.pack(side=LEFT, pady=5, padx=30)
 
-        delete_button = tk.Button(button_frame, text="Delete", command=addbus)
+        delete_button = tk.Button(button_frame, text="Delete", command=delete_record)
         delete_button.pack(side=LEFT, pady=5, padx=30)
+
+        refresh_button = tk.Button(button_frame, text="Refresh", command=refresh_table)
+        refresh_button.pack(side=LEFT, pady=5, padx=30)
 
         con = sqlite3.connect("bus_ticket_DB.db")
         c = con.cursor()
         c.execute("SELECT * FROM bus_details")
         result = c.fetchall()
-
+        
         columns = ("Bus Number", "Plate Number", "Driver Name", "Contact Number", "Capacity", "Destination")
+        global tree
         tree = ttk.Treeview(bus_management_window, columns=columns, show="headings")
 
         for col in columns:
@@ -68,5 +133,7 @@ class bus_management(object):
 
         # Pack the Treeview widget
         tree.pack(expand=True, fill="both")
+
+       
 
         bus_management_window.mainloop()
