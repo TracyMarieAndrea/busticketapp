a18a493cc92faec2b985ecaf9ba80dab75155ddb
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-12-10 10:29:43 +0800

    weeeeeiiii

--- bus_seating.py
+++ bus_seating.py
@@ -40,23 +40,23 @@ class bus_seating(object):
         bottomframe = Frame(window)
         bottomframe.pack()
 
-        seatnum = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"]
+        seatnum_A = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"]
         con = sqlite3.connect('bus_ticket_DB.db')
         c = con.cursor()
 
-        for seat in seatnum:
+        for seat_A in seatnum_A:
             
             c.execute("SELECT Bus_Seating FROM ticket_table WHERE Bus_Destination = ? AND Bus_Time = ? AND Bus_Seating = ?",
-                      (location, time, seat))
+                      (location, time, seat_A))
             result = c.fetchone()
 
             button_color = "green" if result is None else "red"
             
             if(button_color == "green"):
-                redbutton = Button(bottomframe, fg=button_color, text=seat, width=5,command=lambda s=seat: proceed_passenger_form(s, location, time,busno))
+                A_button = Button(bottomframe, fg=button_color, text=seat_A, width=5,command=lambda s=seat_A: proceed_passenger_form(s, location, time,busno))
             else:
-                redbutton = Button(bottomframe, fg=button_color, text=seat, width=5)
-            redbutton.pack( side = LEFT, padx=10, pady=10)
+                A_button = Button(bottomframe, fg=button_color, text=seat_A, width=5)
+            A_button.pack( side = LEFT, padx=10, pady=10)
 
 
 
@@ -64,25 +64,75 @@ class bus_seating(object):
         bottomframe = Frame(window)
         bottomframe.pack()
 
-        seatnum = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"]
+        seatnum_B = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"]
 
 
         con = sqlite3.connect('bus_ticket_DB.db')
         c = con.cursor()
 
-        for seat in seatnum:
+        for seat_B in seatnum_B:
 
             c.execute("SELECT Bus_Seating FROM ticket_table WHERE Bus_Destination = ? AND Bus_Time = ? AND Bus_Seating = ?",
-                      (location, time, seat))
+                      (location, time, seat_B))
             result = c.fetchone()
 
             button_color = "green" if result is None else "red"
 
             if(button_color == "green"):
-                redbutton = Button(bottomframe, fg=button_color, text=seat, width=5,command=lambda s=seat: proceed_passenger_form(s, location, time,busno))
+                B_button = Button(bottomframe, fg=button_color, text=seat_B, width=5,command=lambda s=seat_B: proceed_passenger_form(s, location, time,busno))
             else:
-                redbutton = Button(bottomframe, fg=button_color, text=seat, width=5)
-            redbutton.pack( side = LEFT, padx=10, pady=10)  
+                B_button = Button(bottomframe, fg=button_color, text=seat_B, width=5)
+            B_button.pack( side = LEFT, padx=10, pady=10)  
+        con.close()
+
+
+        bottomframe = Frame(window)
+        bottomframe.pack()
+
+        seatnum_C = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"]
+
+
+        con = sqlite3.connect('bus_ticket_DB.db')
+        c = con.cursor()
+
+        for seat_C in seatnum_C:
+
+            c.execute("SELECT Bus_Seating FROM ticket_table WHERE Bus_Destination = ? AND Bus_Time = ? AND Bus_Seating = ?",
+                      (location, time, seat_C))
+            result = c.fetchone()
+
+            button_color = "green" if result is None else "red"
+
+            if(button_color == "green"):
+                C_button = Button(bottomframe, fg=button_color, text=seat_C, width=5,command=lambda s=seat_C: proceed_passenger_form(s, location, time,busno))
+            else:
+                C_button = Button(bottomframe, fg=button_color, text=seat_C, width=5)
+            C_button.pack( side = LEFT, padx=10, pady=10)  
+        con.close()
+
+
+        bottomframe = Frame(window)
+        bottomframe.pack()
+
+        seatnum_D = ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"]
+
+
+        con = sqlite3.connect('bus_ticket_DB.db')
+        c = con.cursor()
+
+        for seat_D in seatnum_D:
+
+            c.execute("SELECT Bus_Seating FROM ticket_table WHERE Bus_Destination = ? AND Bus_Time = ? AND Bus_Seating = ?",
+                      (location, time, seat_D))
+            result = c.fetchone()
+
+            button_color = "green" if result is None else "red"
+
+            if(button_color == "green"):
+                D_button = Button(bottomframe, fg=button_color, text=seat_D, width=5,command=lambda s=seat_D: proceed_passenger_form(s, location, time,busno))
+            else:
+                D_button = Button(bottomframe, fg=button_color, text=seat_D, width=5)
+            D_button.pack( side = LEFT, padx=10, pady=10)  
         con.close()
 
         window.mainloop()
