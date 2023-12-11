f03d6e6e8eeb775fc0a04134bc6fcc3db1e6e9f8
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-12-11 13:42:50 +0800

    eeeey design config bruh

--- passenger_details_form.py
+++ passenger_details_form.py
@@ -99,37 +99,26 @@ class passenger(object):
         #Bus No. Text
         passenger_name_label = tk.Label(passenger_window,text="Name",font=('Roboto', 12,),fg='black')
         passenger_name_label.pack(pady=10)
-        # bus_no_label.place(rely=0.35,relx=0.60)
-
         #Bus No. Entry
         passenger_name_entry = tk.Entry(passenger_window)
         passenger_name_entry.pack(pady=5)
-        # bus_no_entry.place(rely=0.40,relx=0.60,width=230,height=30)
 
         #Plate Number Text
         address_label = tk.Label(passenger_window,text="Address",font=('Roboto', 12,),fg='black')
         address_label.pack(pady=10)
-        # password_label.place(rely=0.50,relx=0.60)
-
         #Plate Number Entry
         address_entry = tk.Entry(passenger_window)
         address_entry.pack(pady=5)
-        # password_entry.place(rely=0.55,relx=0.60,width=230,height=30)
 
         #Driver Name Text
         contact_no_label = tk.Label(passenger_window,text="Contact Number",font=('Roboto', 12,),fg='black')
         contact_no_label.pack(pady=10)
-
         #Driver Name Entry
         contact_no_entry = tk.Entry(passenger_window)
         contact_no_entry.pack(pady=5)
 
-
         #Save and Ticket Button
         save_btn = tk.Button(passenger_window,text="T I C K E T",background="#52ACA1",border=0, command=save_passenger_details)
         save_btn.pack(pady=10)
-        # save_btn.place(rely=0,relx=0,height=25,width=230)
-
-
 
         passenger_window.mainloop()
